35263fa5318db7aafffa72a4c620f00f126b0e3f
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2021-03-31 23:58:14 +0500

    trash site

---
+++ js/app.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+import 'slick-carousel';
+import IMask from 'imask';
+
+require('./modules/mobile-menu');
+require('./modules/popup');
+require('./modules/lazyload');
+
+
+$(document).ready(function () {
+	$('.slider').slick();
+
+	IMask(document.querySelector('.popup__tel'), {
+		mask: '+{7}(000)000-00-00'
+	});
+
+});
